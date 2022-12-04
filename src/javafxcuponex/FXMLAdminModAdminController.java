3040fd9d0fe6ab66b70a033b7d99515e4b5346a9
andreszaraterodriguez <andres.zarate162ver@gmail.com>
2022-12-03 23:29:24 -0600

    add

--- src/javafxcuponex/FXMLAdminModAdminController.java
+++ src/javafxcuponex/FXMLAdminModAdminController.java
@@ -5,17 +5,30 @@
  */
 package javafxcuponex;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafxcuponex.modelo.ConsumirServiciosWeb;
 import javafxcuponex.pojos.Administrador;
+import javafxcuponex.util.Constantes;
+import javafxcuponex.util.Utilidades;
 
 /**
  * FXML Controller class
@@ -50,7 +63,21 @@ public class FXMLAdminModAdminController implements Initializable {
         colNombre.setCellValueFactory(new PropertyValueFactory ("nombre"));
         colApellidoP.setCellValueFactory(new PropertyValueFactory("apellidoP"));
         colApellidoM.setCellValueFactory(new PropertyValueFactory ("apellidoM"));
-        
+        colCorreo.setCellValueFactory(new PropertyValueFactory("correo"));
+    }
+    private void cargarInfoUsuarioWS(){
+        String urlWS = Constantes.URL_BASE+"Administrador/all";
+        try{
+            String jsonRespuesta = ConsumirServiciosWeb.consumirServicioGET(urlWS);
+            Gson gson = new Gson();
+            Type tipoListaAdministrador =  new TypeToken<List<Administrador>>(){}.getType(); 
+            List usuarioWS = gson.fromJson(jsonRespuesta, tipoListaAdministrador);
+            listaAdministrador.addAll(usuarioWS);
+            tbUsuarios.setItems(listaAdministrador);
+            
+        }catch (Exception e){
+            Utilidades.mostrarAlertaSimple("Error en conexion ", "Error al consultar", Alert.AlertType.ERROR);
+        }
     }
 
     @FXML
@@ -63,6 +90,16 @@ public class FXMLAdminModAdminController implements Initializable {
 
     @FXML
     private void clickAgregarUsuario(ActionEvent event) {
+        try{
+            Parent vista = FXMLLoader.load(getClass().getResource("FXMLModAdmin.fxml"));
+            Scene escenaFormulario = new Scene(vista);
+            Stage escenarioFormulario = new Stage();
+            escenarioFormulario.setScene(escenaFormulario);
+            escenarioFormulario.initModality(Modality.APPLICATION_MODAL);
+            escenarioFormulario.showAndWait();
+        } catch(Exception e){
+            
+        }
     }
     
 }
